6c75b1ce828d2c9731b747b25972e06422211ed6
Bryant Luu <bryant@numer.ai>
2019-11-30 13:36:31 -0800

    363 - 374

---
+++ 368.py
@@ -0,0 +1,36 @@
+"""
+Good morning! Here's your coding interview problem for today.
+
+This problem was asked by Google.
+
+Implement a key value store, where keys and values are integers, with the following methods:
+
+update(key, vl): updates the value at key to val, or sets it if doesn't exist
+get(key): returns the value with key, or None if no such value exists
+max_key(val): returns the largest key with value val, or None if no key with that value exists
+For example, if we ran the following calls:
+
+kv.update(1, 1)
+kv.update(2, 1)
+And then called kv.max_key(1), it should return 2, since it's the largest key with value 1.
+"""
+
+
+class KeyValueStore():
+    def __init__(self):
+        self.store = {}
+
+    def get(self, key):
+        if key in self.store:
+            return self.store[key]
+        return None
+
+    def update(self, key, value):
+        self.store[key] = value
+
+    def max_key(self, value):
+        keys = []
+        for key in self.store:
+            if self.store[key] == value:
+                keys.append(key)
+        return max(keys)
